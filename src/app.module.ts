3a78f222fdee35d768857344312d664985903eaf
selveresta <artem.lypnyk@softsculptor.net>
2023-07-27 15:38:39 +0300

    prepate project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LoggerModule } from './shared/logger/logger.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './entities/user/user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '../configs/.env.development'
+        }),
+        SequelizeModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                dialect: 'postgres',
+                host: configService.get('HOST'),
+                port: +configService.get('PORT'),
+                username: configService.get('USERNAME'),
+                password: configService.get('PASSWORD'),
+                database: configService.get('DATABASE'),
+                models: []
+            })
+        }),
+        LoggerModule,
+        AuthModule,
+        UserModule
+    ],
+    controllers: [],
+    providers: []
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerService } from './shared/logger/logger.service';
+
+const PORT = process.env.PORT || 8555;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {
+        bufferLogs: true
+    });
+    app.useLogger(app.get(LoggerService));
+
+    await app.listen(PORT, onStart);
 }
+
+const onStart = () => {
+    const logger = new LoggerService();
+    logger.log(`Server start on port: ${PORT}`);
+};
+
 bootstrap();
